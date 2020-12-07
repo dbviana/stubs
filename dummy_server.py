eabbcb09c53ce00e0e401563a59adc7c4753a6f2
David Viana <dbviana733@gmail.com>
2020-12-07 13:44:09 +0000

    One socket per stub, info logging, handling Ctrl-C, battery trait, server threading

--- dummy_server.py
+++ dummy_server.py
@@ -9,50 +9,88 @@ import argparse
 from ipaddress import ip_address
 import socket
 import struct
-from socketserver import TCPServer, BaseRequestHandler
+import threading
 
 
-class Stub:
-    def __init__(self, ip, port):
-        self.ip = ip
-        self.port = port
-
-
-class DummyTCPHandler(BaseRequestHandler):
+class ClientThread(threading.Thread):
     """
-    A Dummy TCP Handler
-    Prints to the console whatever it receives
+    Handles communication with stub
     """
 
-    def handle(self):
-        self.unpacker = struct.Struct("I f")
-        self.data = self.request.recv(self.unpacker.size)
-        print("{} wrote:".format(self.client_address[1]))
+    def __init__(self, _sock, _ip, _port):
+        threading.Thread.__init__(self, daemon=True)
+        self.sock = _sock
+        self.client_ip = _ip
+        self.client_port = _port
+        print("[+] New connection: {} {}".format(self.client_ip, self.client_port))
 
-        try:
-            info = self.unpacker.unpack(self.data)
-            print(info)
-        except Exception as e:
-            print(e)
+        self.unpacker = struct.Struct("I f f")
+        self.exit = False
+
+    def run(self):
+        while not self.exit:
+            data = self.sock.recv(self.unpacker.size)
+            # print("Recv data: {}".format(data))
+            if len(data) == 0:
+                # Connection terminated
+                print(
+                    "[-] Connection terminated: {} {}".format(
+                        self.client_ip, self.client_port
+                    )
+                )
+                break
+
+            info = self.unpacker.unpack(data)
+            for val in info:
+                print("{:.2f} ".format(val), end="")
+            print("")
+
+        print("[X] Closing socket: {} {}".format(self.client_ip, self.client_port))
+        self.sock.shutdown(socket.SHUT_RDWR)
+        self.sock.close()
 
 
 parser = argparse.ArgumentParser(
-    prog="stub", description="Simulate a simple stateful EV charger."
+    prog="stub",
+    description="Dummy server for testing the simulation of a simple EV charger.",
 )
 parser.add_argument("--my_addr", type=ip_address, required=True, help="my address")
 parser.add_argument("--my_port", type=int, default=12345, help="my port")
 args = parser.parse_args()
 
 
-class MyTCPServer(TCPServer):
-    def server_bind(self):
-        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.socket.bind(self.server_address)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.settimeout(1)
+sock.bind((str(args.my_addr), args.my_port))
+# sock.bind((socket.get_hostname(), args.my_port))
 
+sock.listen(5)
 
-server = MyTCPServer((str(args.my_addr), args.my_port), DummyTCPHandler)
+threads = []
+
+CLIENT_SOCK = None
+IP = None
+PORT = None
 
 try:
-    server.serve_forever()
+    while True:
+        try:
+            CLIENT_SOCK, (IP, PORT) = sock.accept()
+        except socket.timeout:
+            continue
+
+        new_thr = ClientThread(CLIENT_SOCK, IP, PORT)
+        new_thr.start()
+        threads.append(new_thr)
+
 except KeyboardInterrupt:
-    server.shutdown()
+    print("Exiting!")
+
+    for thr in threads:
+        thr.exit = True
+
+    sock.close()
+
+    for thr in threads:
+        thr.join(1)
--- stub.py
+++ stub.py
@@ -13,7 +13,7 @@ import time
 
 
 parser = argparse.ArgumentParser(
-    prog="stub", description="Simulate a simple stateful EV charger."
+    prog="stub", description="Simulate a simple EV charger."
 )
 parser.add_argument(
     "--server_addr", type=ip_address, required=True, help="server address"
@@ -24,46 +24,33 @@ args = parser.parse_args()
 print(args.server_addr)
 
 
-class Stub:
-    def __init__(
-        self, id, cap=6.1, perc=0.2, falloff=0.7, min_power=5, max_power=62.5,
-    ):
-        self.id = id
+class Battery:
+    """
+    Encapsulates the characteristics and
+    behavior of an EV battery
+    """
+
+    def __init__(self, cap, perc, falloff, power_bounds):
         self.battery_capacity = cap
         self.perc = perc
         self.current_battery = perc * cap
 
         self.falloff_point = falloff
-        self.min_power = min_power
-        self.max_power = max_power
+        self.min_power = power_bounds[0]
+        self.max_power = power_bounds[1]
 
-        self.charging = False
-        self.power_intake = None
-        self.available_power = None
-
-        self._init_socket()
-
-    def _init_socket(self):
-        self.sock = socket.socket()
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.sock.connect((str(args.server_addr), args.server_port))
-
-    def start_charging_at(self, available_power):
-        self.charging = True
-        self.available_power = available_power
-
-        self._calculate_power_intake()
-
-    def _calculate_power_intake(self):
-        def lerp_power(perc):
-            return self.max_power + perc * (self.min_power - self.max_power)
+    def calculate_max_power_intake(self):
+        """
+        Calculates max power intake at a certain battery
+        level
+        """
 
         if self.perc < self.falloff_point:
-            self.power_intake = min(self.max_power, self.available_power)
-        else:
-            self.power_intake = min(lerp_power(self.perc), self.available_power)
+            return self.max_power
 
-    def charge(self, delta_t):
+        return self.max_power + self.perc * (self.min_power - self.max_power)
+
+    def charge(self, delta_t, power_intake):
         """
         Simple charge curve
         Charges at full speed until a certain
@@ -72,57 +59,125 @@ class Stub:
         charging speed at 100%. Must also take
         into account the available power.
         """
-
-        self._calculate_power_intake()
-        self.current_battery += self.power_intake * delta_t
+        self.current_battery += power_intake * delta_t
         self.perc = self.current_battery / self.battery_capacity
 
         if self.perc >= 1:
             self.perc = 1
             self.current_battery = self.battery_capacity
-            self.charging = 0
-
-    def inform_server(self):
-        values = (self.id, self.power_intake)
-        packer = struct.Struct("I f")
-        packed_data = packer.pack(*values)
-
-        try:
-            print("Sending packet... (ID:{}, {}kWh)".format(*values))
-            self.sock.send(packed_data)
-        except ConnectionAbortedError:
-            pass
-
-        self.sock.close()
-        self._init_socket()
 
     def __repr__(self):
-        s = "Stub no. {}: ".format(self.id)
-
-        if self.charging:
-            s += "Charging @{:.2f}kWh ".format(self.power_intake)
-        else:
-            s += "Idle "
-
-        s += "({:.2f}/{:.2f}kWh [{:.2f}%])".format(
+        return "({:.2f}/{:.2f}kWh [{:.2f}%])".format(
             self.current_battery, self.battery_capacity, self.perc * 100
         )
 
-        return s
+
+default_battery = Battery(cap=6.1, perc=0.2, falloff=0.7, power_bounds=(5, 62.5))
 
 
-# sock.sendall(b"abcde")
-# buf = sock.recv(64)
+class Stub:
+    """
+    Simulates a single charger unit
+    """
+
+    def __init__(self, ID, battery=default_battery):
+        self.identifier = ID
+        self.battery = battery
+
+        self.charging = False
+        self.power_intake = None
+        self.available_power = None
+
+        self.packer = struct.Struct("I f f")
+
+        self._init_socket()
+
+    def _init_socket(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((str(args.server_addr), args.server_port))
+
+    def start_charging_at(self, available_power):
+        """
+        Sets up charging info
+        """
+        self.charging = True
+        self.available_power = available_power
+        self.calculate_power_intake()
+
+    def calculate_power_intake(self):
+        """
+        Calculates power intake given the current
+        available power and battery level
+        """
+        self.power_intake = min(
+            self.battery.calculate_max_power_intake(), self.available_power
+        )
+
+    def charge(self, delta_t):
+        """
+        Simulate charging the battery
+        """
+
+        self.calculate_power_intake()
+        self.battery.charge(delta_t, self.power_intake)
+        if self.battery.perc == 1.0:
+            self.charging = 0
+
+    def inform_server(self):
+        """
+        Informs the server of the stub's
+        current status
+        """
+        values = (self.identifier, self.power_intake, self.battery.perc * 100)
+        packed_data = self.packer.pack(*values)
+
+        # print("Sending packet... (identifier:{}, {:.2f}kWh, {:.2f}%)".format(*values))
+        self.sock.send(packed_data)
+
+    def disconnect(self):
+        """
+        Disconnects from the server and
+        cleans up
+        """
+        self.sock.shutdown(socket.SHUT_RDWR)
+        self.sock.close()
+
+    def __repr__(self):
+        return (
+            "Stub no. {}: ".format(self.identifier)
+            + str(self.battery)
+            + (
+                " Charging @{:.2f}kWh".format(self.power_intake)
+                if self.charging
+                else " Idle"
+            )
+        )
+
 
 stub0 = Stub(0)
 
 stub0.start_charging_at(100)
 
-time_speed = 100
-time_sleep = 0.1
+TIME_SPEED = 100
+TIME_SLEEP = 1
+TIME_ELAPSED = 0
 
-while stub0.charging:
+while True:
     print(stub0)
-    stub0.charge(time_sleep * time_speed / 3600)
-    stub0.inform_server()
-    time.sleep(time_sleep)
+
+    try:
+        stub0.inform_server()
+    except ConnectionAbortedError:
+        print("[!] Server must be down: exiting!")
+        break
+
+    time.sleep(TIME_SLEEP)
+    TIME_ELAPSED += TIME_SLEEP
+
+    stub0.charge(TIME_SLEEP * TIME_SPEED / 3600)
+
+    if TIME_ELAPSED > 10:
+        break
+
+
+stub0.disconnect()
